6ec2bc16d0ecf5486ecf571ed75c966f1e161efb
ryota_miyagawa <i31640.ryota@gmail.com>
2025-04-18 16:07:39 +0900

    fix:QuizTitleの色指定をコンポーネント内で完結させるようにしたため色指定を削除

--- src/components/QuizLengthSelector.tsx
+++ src/components/QuizLengthSelector.tsx
@@ -8,7 +8,7 @@ interface QuizLengthSelectorProps {
 export function QuizLengthSelector({ onSelectLength }: QuizLengthSelectorProps) {
   return (
     <Container size="sm" py="xl">
-      <QuizTitle color="white" />
+      <QuizTitle />
       <Card shadow="sm" padding="lg" radius="md" withBorder>
         <Stack align="center" gap="md">
           <Title order={2}>問題数を選択してください</Title>
